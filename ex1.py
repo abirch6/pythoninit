403341ad5650fdb923ff6618abdda10464d88f95
abirch6 <77842847+abirch6@users.noreply.github.com>
2023-06-29 16:31:52 +0200

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,22 @@
+# demander à l'utilisateur d'entrer deux nombres
+nb = float(input("donner le premier nombre: "))
+nb1 = float(input("donner le deuxième nombre: "))
+
+# calcul de la somme
+somme = nb + nb1
+print("somme:", somme)
+
+#calcul de difference
+
+difference = nb - nb1
+print("difference:", difference)
+
+#calcul de produit
+
+produit= nb*nb1
+print("produit:", produit)
+
+# calcul de quotient
+
+quotient = nb / nb1
+print("quotient:", quotient)
